8c0e9b1c089fb44c48c57703dd64fc26e01f7106
DNNguyen-2003 <97666224+DNNguyen-2003@users.noreply.github.com>
2022-02-18 08:41:59 +0700

    Add files via upload

---
+++ hoclaptrinh.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package codeJava;
+import java.util.Scanner;
+/**
+ *
+ * @author 84705
+ */
+public class hoclaptrinh {
+    public static void main (String[] args) {
+        double r;
+        Scanner input = new Scanner(System.in);
+        r = input.nextDouble();
+        System.out.print("Chu vi duong tron la: "+ (r*r*3.14159));
+    }    
+}
