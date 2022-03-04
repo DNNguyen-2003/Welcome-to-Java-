ff587fd1c4465c2b061d698c68313e1e4dd73633
DNNguyen-2003 <dnnguyen3152003@gmail.com>
2022-03-04 18:58:38 +0700

    update

---
+++ PhuongTrinhBacHai.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package baitap;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 84705
+ */
+public class PhuongTrinhBacHai {
+    public static void main (String[] args){
+        try{ float a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập A: ");
+        a = sc.nextFloat();
+        System.out.print("Nhập B: ");
+        b = sc.nextFloat();
+        System.out.print("Nhập C: ");
+        c = sc.nextFloat();
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phương trình vô nghiệm!");
+            } else {
+                System.out.println("Phương trình có một nghiệm: "
+                        + "x = " + (-c / b));
+            }
+            return;
+        }
+        float delta = b*b - 4*a*c;
+        float x1;
+        float x2;
+        if (delta > 0) {
+            x1 = (float) ((-b + Math.sqrt(delta)) / (2*a));
+            x2 = (float) ((-b - Math.sqrt(delta)) / (2*a));
+            System.out.println("Phương trình có 2 nghiệm là: "
+                    + "x1 = " + x1 + " và x2 = " + x2);
+        } else if (delta == 0) {
+            x1 = (-b / (2 * a));
+            System.out.println("Phương trình có nghiệm kép: "
+                    + "x1 = x2 = " + x1);
+        } else {
+            System.out.println("Phương trình vô nghiệm!");
+        }
+        }catch (Exception e)
+        {
+            System.out.println("Không hợp lệ !!!");
+        }
+              }
+    }
